b8c9b9a4d5f9d97be70114fe4ce0d1dbc509dac2
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2011-09-07 11:57:52 +0200

    Added single view base project

---
+++ Marijn/IB/week 2/week 2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  week 2
+//
+//  Created by Marijn van der Werf on 07-09-11.
+//  Copyright (c) 2011 Fontys Hogeschool ICT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
