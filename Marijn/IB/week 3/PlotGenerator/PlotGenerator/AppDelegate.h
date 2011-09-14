cf3c3b7a880c5ecb152342d06a707a85d0ea6d28
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2011-09-14 11:21:50 +0200

    Created PlotGenerator project

---
+++ Marijn/IB/week 3/PlotGenerator/PlotGenerator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlotGenerator
+//
+//  Created by Marijn van der Werf on 14-09-11.
+//  Copyright (c) 2011 Fontys Hogeschool ICT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Marijn/IB/week 3/PlotGenerator/PlotGenerator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PlotGenerator
+//
+//  Created by Marijn van der Werf on 14-09-11.
+//  Copyright (c) 2011 Fontys Hogeschool ICT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
