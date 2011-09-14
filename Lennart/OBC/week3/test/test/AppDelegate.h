33a10eb5607a8ee56fd271af4d4f84276ffcbb7e
Lennart van Mourik <lennartvm@gmail.com>
2011-09-14 22:30:44 +0200

    test file

---
+++ Lennart/OBC/week3/test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Lennart van Mourik on 14-09-11.
+//  Copyright (c) 2011 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lennart/OBC/week3/test/test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Lennart van Mourik on 14-09-11.
+//  Copyright (c) 2011 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
